bbd1a6f7cddb6a41f80a4c4be4b222037dc4e31d
minhtam <zanname98@gmail.com>
2023-03-25 09:18:38 +0700

    Portfolio

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,17 @@
 const images = {
     avatar: require('~/assets/images/avatar.jpg'),
     banner: require('~/assets/images/banner.png'),
+    //Portfolio
+    image1: require('~/assets/images/image-1.png'),
+    image2: require('~/assets/images/image-2.png'),
+    image3: require('~/assets/images/image-3.png'),
+    image4: require('~/assets/images/image-4.png'),
+    image5: require('~/assets/images/image-5.png'),
+    image6: require('~/assets/images/image-6.png'),
+    image7: require('~/assets/images/image-7.png'),
+    image8: require('~/assets/images/image-8.png'),
+    image9: require('~/assets/images/image-9.png'),
+    rectangle: require('~/assets/images/rectangle.png'),
 };
 
 export default images;
--- src/pages/Portfolio/index.js
+++ src/pages/Portfolio/index.js
@@ -1,5 +1,70 @@
-function Pofolio() {
-    return <h1>Pofolio</h1>;
+import classNames from 'classnames/bind';
+import images from '~/assets/images';
+import Styles from './Portfolio.module.scss';
+
+const cx = classNames.bind(Styles);
+function Portfolio() {
+    return (
+        <div className={cx('wrapper')}>
+            <div className={cx('title')}>
+                <h2>Pofolio</h2>
+                <p>
+                    Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat
+                    duis enim velit mollit. lorem ipsum
+                </p>
+            </div>
+            <div className={cx('container')}>
+                <div className={cx('list-menu')}>
+                    <ul>
+                        <li>
+                            <a>All categories</a>
+                        </li>
+                        <li>
+                            <a>UI Design</a>
+                        </li>
+                        <li>
+                            <a>Web Templates</a>
+                        </li>
+                        <li>
+                            <a>Logo</a>
+                        </li>
+                        <li>
+                            <a>Branding</a>
+                        </li>
+                    </ul>
+                </div>
+                <div className={cx('card-item')}>
+                    <div className={cx('image-item')}>
+                        <img src={images.image1} />
+                    </div>
+                    <div className={cx('image-item')}>
+                        <img src={images.image2} />
+                    </div>
+                    <div className={cx('image-item')}>
+                        <img src={images.image3} />
+                    </div>
+                    <div className={cx('image-item')}>
+                        <img src={images.image4} />
+                    </div>
+                    <div className={cx('image-item')}>
+                        <img src={images.image5} />
+                    </div>
+                    <div className={cx('image-item')}>
+                        <img src={images.image6} />
+                    </div>
+                    <div className={cx('image-item')}>
+                        <img src={images.image7} />
+                    </div>
+                    <div className={cx('image-item')}>
+                        <img src={images.image8} />
+                    </div>
+                    <div className={cx('image-item')}>
+                        <img src={images.image9} />
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
-export default Pofolio;
+export default Portfolio;
